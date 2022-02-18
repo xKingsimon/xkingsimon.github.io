4c99343cd050bed6dbc34101afb14cdc3dc6fc30
xKingsimon <55033367+xKingsimon@users.noreply.github.com>
2022-02-18 13:30:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 $(document).ready(function () {
-  var navbbox_bot = $("#navbar").getBoundingClientRect().height;
+  var navbbox_bot = document.getElementById("navbar").getBoundingClientRect().height;
+  changeArrowDist();
+  var navHeight = $("#navbar").outerHeight();
   $(window).scroll(function () {
     if ($(document).scrollTop() >= navbbox_bot) {
-      $("#navbar").addClass("sticky");
-      $("#navbarPlaceholder").css("height", navbbox_bot);
-    } else {
+      if (!$("#navbar").hasClass("sticky"))
+      {
+        $("#navbar").css("top",-navHeight + "px");
+        console.log("set top to: "+-navHeight+"px");
+        $("#navbar").addClass("sticky");
+        $("#navbarPlaceholder").css("height", navbbox_bot);
+        $("#navbar").animate({top: "0px"}, 300);
+      }
+        } else if($(document).scrollTop() == 0) {
       $("#navbar").removeClass("sticky");
       $("#navbarPlaceholder").css("height", 0);
     }
   });
 });
+
+$(window).resize(changeArrowDist);
 function changeArrowDist() {
-  var dist =
-    $("#computer2").getBoundingClientRect().left -
-    $("#svg-triangle1").getBoundingClientRect().right;
-  root.style.setProperty("--arrow_dist", dist);
-  
-  console.log(dist);
+  var dist = document.getElementById("computer2bbox").getBoundingClientRect().left - document.getElementById("svg-triangle3").getBoundingClientRect().right;
+  document.documentElement.style.setProperty("--arrow_dist", String(Math.max(0,dist-10))+ "px");
 }
