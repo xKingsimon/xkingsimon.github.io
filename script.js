cfcfd27d6d29c59e80ca9cf06281b9d9321a2540
xKingsimon <55033367+xKingsimon@users.noreply.github.com>
2022-02-18 13:32:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   var navbbox_bot = document.getElementById("navbar").getBoundingClientRect().height;
-  changeArrowDist();
   var navHeight = $("#navbar").outerHeight();
   $(window).scroll(function () {
     if ($(document).scrollTop() >= navbbox_bot) {
@@ -18,9 +17,3 @@ $(document).ready(function () {
     }
   });
 });
-
-$(window).resize(changeArrowDist);
-function changeArrowDist() {
-  var dist = document.getElementById("computer2bbox").getBoundingClientRect().left - document.getElementById("svg-triangle3").getBoundingClientRect().right;
-  document.documentElement.style.setProperty("--arrow_dist", String(Math.max(0,dist-10))+ "px");
-}
