e6b6e4a1330fe8d7e92dec110bc5c91a91d8677e
BuildTools <unconfigured@null.spigotmc.org>
2022-02-22 20:36:03 +0100

    a

--- global/script.js
+++ global/script.js
@@ -3,12 +3,14 @@ $(document).ready(function() {
         $(window).resize(changeNavSize);
         changeNavSize();
         $(window).scroll(function() {
+            console.log("Current X: "+$(document).scrollTop())
+            console.log("Placeholder X: "+navbbox_bot)
             if ($(document).scrollTop() >= navbbox_bot) {
                 if (!$("#navbar").hasClass("sticky")) {
                     $("#navbar").addClass("sticky");
                     $("#navbarPlaceholder").css("height", navHeight);
                 }
-            } else if ($(document).scrollTop() <= navbbox_bot) {
+            } else if ($(document).scrollTop() < navbbox_bot) {
                 $("#navbar").removeClass("sticky");
                 $("#navbarPlaceholder").css("height", 0);
             }
@@ -17,11 +19,10 @@ $(document).ready(function() {
 });
 
 function href(url) {
-    console.log("link to" + url)
     window.location.replace(url);
 }
 
 function changeNavSize() {
-    navbbox_bot = document.getElementById("navbar").getBoundingClientRect().top;
+    navbbox_bot = document.getElementById("navbarPlaceholder").getBoundingClientRect().bottom + window.scrollY;
     navHeight = $("#navbar").outerHeight();
 }
