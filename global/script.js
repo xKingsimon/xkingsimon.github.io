60750525f68844440dd2a3bb5e94d6508235db1a
BuildTools <unconfigured@null.spigotmc.org>
2022-02-22 18:23:05 +0100

    a

--- global/script.js
+++ global/script.js
@@ -1,25 +1,27 @@
-$(document).ready(function () {
-  $("#importNavbar").load("/global/navbar.html", function() {
-    var navbbox_bot = document.getElementById("navbar").getBoundingClientRect().height;
-    var navHeight = $("#navbar").outerHeight();
-    $(window).scroll(function () {
-      if ($(document).scrollTop() >= navbbox_bot) {
-        if (!$("#navbar").hasClass("sticky"))
-        {
-          $("#navbar").css("top",-navHeight + "px");
-          console.log("set top to: "+-navHeight+"px");
-          $("#navbar").addClass("sticky");
-          $("#navbarPlaceholder").css("height", navbbox_bot);
-          $("#navbar").animate({top: "0px"}, 300);
-        }
-          } else if($(document).scrollTop() == 0) {
-        $("#navbar").removeClass("sticky");
-        $("#navbarPlaceholder").css("height", 0);
-      }
+$(document).ready(function() {
+    $("#importNavbar").load("/global/navbar.html", function() {
+        $(window).resize(changeNavSize);
+        changeNavSize();
+        $(window).scroll(function() {
+            if ($(document).scrollTop() >= navbbox_bot) {
+                if (!$("#navbar").hasClass("sticky")) {
+                    $("#navbar").addClass("sticky");
+                    $("#navbarPlaceholder").css("height", navHeight);
+                }
+            } else if ($(document).scrollTop() <= navbbox_bot) {
+                $("#navbar").removeClass("sticky");
+                $("#navbarPlaceholder").css("height", 0);
+            }
+        });
     });
-  });
 });
+
 function href(url) {
-  console.log("link to" + url)
-  window.location.replace(url);
+    console.log("link to" + url)
+    window.location.replace(url);
+}
+
+function changeNavSize() {
+    navbbox_bot = document.getElementById("navbar").getBoundingClientRect().top;
+    navHeight = $("#navbar").outerHeight();
 }
