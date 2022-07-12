a56037a2157421099a24ff151bbad3ed97651312
BuildTools <unconfigured@null.spigotmc.org>
2022-07-12 12:57:17 +0200

    a

--- simon/mathe/scripts/aufgaben.js
+++ simon/mathe/scripts/aufgaben.js
@@ -2,9 +2,12 @@ currentFunctionLength = 0;
 expectedFunctionLength = 4;
 func = 0;
 diff = 0;
+bestListMap = "";
+db = new restdb("62cc9744cba57849d7caf95d");
+namesArray = [[],[],[]];
 $(window).on('load', function () {
     newFunction();
-
+    loadScoreBoard();
     $("#length-number").on("input", function() {
         expectedFunctionLength = this.value;
     });
@@ -15,6 +18,7 @@ $(window).on('load', function () {
         var input = $("#answer-field").val();
         if(math.symbolicEqual(input,diff)) {
             newFunction();
+            addScore(1);
         }
     });
     $("#answer-field").keydown(function (e) {
@@ -22,6 +26,7 @@ $(window).on('load', function () {
         if (e.which == 13) {
             if(math.symbolicEqual(input,diff)) {
                 newFunction();
+                addScore(1);
             }
         }
     });
@@ -111,4 +116,88 @@ function newFunction() {
     diff = math.derivative(func,"x");
     $("#question2").text("$$"+math.parse(diff.toString()).toTex()+"$$");
     MathJax.typesetPromise();
+}
+
+function addScore(points) {
+    var input = $("#name-input").val();
+    if (input != "none") {
+        if (namesArray[1].includes(input)) {
+            console.log("update");
+            var position = $.inArray(String(input),namesArray[1])
+            console.log(position)
+            if (position > -1)
+            {
+                id = namesArray[0][position];
+            } else {
+                id = 0
+                alert("id not found");
+            }
+            console.log(id)
+            var jsondata = {"score": namesArray[2][position]+points};
+            $.ajax({
+                url: "https://johnsdorfer-3d8a.restdb.io/rest/ableitungsscore/"+id,
+                beforeSend: function(xhr) { 
+                    xhr.setRequestHeader('x-apikey', '62cc9744cba57849d7caf95d');
+                },
+                headers: {
+                    "content-type": "application/json",
+                    "x-apikey": "62cc9744cba57849d7caf95d",
+                    "cache-control": "no-cache"
+                },
+                type: 'Put',
+                data: JSON.stringify(jsondata),
+                success: function () {
+                    loadScoreBoard();
+                },
+                error: function(){
+                  alert("Cannot send data");
+                }
+            });
+        } else {
+            console.log("add");
+            var jsondata = {"username":String(input),"score":String(points)};
+            $.ajax({
+                url: "https://johnsdorfer-3d8a.restdb.io/rest/ableitungsscore",
+                beforeSend: function(xhr) { 
+                    xhr.setRequestHeader('x-apikey', '62cc9744cba57849d7caf95d');
+                },
+                headers: {
+                    "content-type": "application/json",
+                    "x-apikey": "62cc9744cba57849d7caf95d",
+                    "cache-control": "no-cache"
+                },
+                type: 'Post',
+                data: JSON.stringify(jsondata),
+                success: function () {
+                    loadScoreBoard();
+                },
+                error: function(){
+                  alert("Cannot send data");
+                }
+            });
+        }
+    }
+}
+function loadScoreBoard() {
+    console.log("loading scores");
+    var query = {};
+    var hints = {"$orderby": {"score": -1}};
+    db.ableitungsscore.find(query, hints, function(err, res){
+        if (!err){
+            // res is an array of ableitungsscore instances
+            for(i=0;i<res.length;i++)
+            {
+                currentPlayerMap = new Map(Object.entries(res[i]));
+                console.log("currentPlayerArray")
+                console.log(currentPlayerMap)
+                namesArray[0].push(currentPlayerMap.get("_id"));
+                namesArray[1].push(currentPlayerMap.get("username"));
+                namesArray[2].push(currentPlayerMap.get("score"));
+                $("#wrapper").append("<p class='score-new'>"+currentPlayerMap.get("username")+": "+currentPlayerMap.get("score")+"</p>");
+            }
+            $(".score").remove();
+            $(".score-new").attr("class","score");
+            console.log(namesArray);
+        }
+    });
 }
